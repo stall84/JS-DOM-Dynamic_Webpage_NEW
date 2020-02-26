afb864de274f7c665a9bbeca3b61219d9f98f45a
Michael Stallings <stall84@gmail.com>
2020-02-26 18:41:41 -0500

    made some progress toward completing all sections. ran into difficulty for some unknown reason with bootstrap's margins/padding

--- js/script.js
+++ js/script.js
@@ -7,11 +7,18 @@
 
 // grab the only container hard-coded in the html to start work inside the document
 const mainContainer = document.getElementById('container');
+//mainContainer.classList.add('container','row');
 
 // attach the function that will create the header to the mainContainer div
 mainContainer.appendChild(createHeader());
 
+mainContainer.appendChild(heroMaker());
+
+mainContainer.appendChild(posts());
+
 // In this method we will be wrapping everything in functions to make them available immediately and for usability later on if needed
+// Also instead of separating building parts of the code and then appending parts of the code. We will simply code things out 
+// as they would linearly arise on the page
 
 function createHeader() {
 
@@ -23,27 +30,68 @@ function createHeader() {
     headerContainer.style.color = 'white';
     headerContainer.style.backgroundColor = '#57a9f7';
     headerContainer.style.padding = '2em';
-    headerContainer.style.margin = '2em';
+    headerContainer.style.margin = '2.1em';
+    //headerContainer.style.margin = '1em';
     
 
     
 
     // nav title 
     const pageTitle = document.createElement('h1');
+    // using innerText method instead of createTextNode
     pageTitle.innerText = "HighOnCoding";
+    pageTitle.style.fontSize = "3.1em";
     headerContainer.appendChild(pageTitle);
 
     // nav links 
     const linksList = document.createElement('div');
+    linksList.style.fontSize = "1.35em";
     const homeLink = document.createElement('a');
     const catsLink = document.createElement('a');
-    //homeLink.innerText = 'Home';
-    homeLink.innerHTML = '<a href="#Home">Home</a>';
-    homeLink.style.margin = '2em';
-    catsLink.innerText = 'Categories';
+    // create inner HTML with anchor tag and placheholder link. override default styling colors for links
+    homeLink.innerHTML = '<a style="color:white"href="#Home">Home</a>';
+    // various stylings to try and separate the two link elements, and remove default link styling (underline/blue) using relative elements for speed
+    homeLink.style.marginLeft = '4em';
+    catsLink.innerHTML = '<a style="color:white"href="#Cats">Categories</a>';
+    catsLink.style.marginLeft = '3em';
     linksList.appendChild(homeLink);
     linksList.appendChild(catsLink);
     headerContainer.appendChild(linksList);
 
     return headerContainer;
 };
+
+// Create the (static/no parameters) function that will make the hero-section of the page. This won't be changeable like the review/posts will be below
+
+function heroMaker() {
+    // create container div for the hero content
+    const heroContainer = document.createElement('div');
+    // add bootstrap classes to hero container just created
+    heroContainer.classList.add('container', 'row', 'm-auto');
+    heroContainer.style.backgroundColor = '#dddee0';
+    const heroH1 = document.createElement('h1');
+    const heroPara = document.createElement('p');
+    // add text content and specific (inline) stylings to the elements created
+    heroH1.innerText = 'Curse of the Current Reviews';
+    heroH1.style.color = '#53585e';
+    heroH1.style.padding = '.2em';
+    heroPara.innerText = 'When you buy an application from the Apple iTunes store you have more choices than you can handle. Most of the users scroll past the application description completely avoiding it like ads displayed on the right column of your webpage. Their choice is dependent on three important factors price, screenshot and reviews';
+    heroPara.style.marginTop = '.5em';
+    heroPara.style.paddingLeft = '.4em';
+    
+    heroContainer.appendChild(heroH1);
+    heroContainer.appendChild(heroPara);
+
+    return heroContainer;
+}
+
+
+
+// We will create a single function that can/will be used to make the posts section(s), since they will all use the same 
+// formats and stylings, just different content added.
+
+function postMaker() {
+
+
+
+}
